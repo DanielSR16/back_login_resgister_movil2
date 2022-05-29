b129150dbc270f6467aede3d754c28c654a84263
DANIEL SANCHEZ ROBLES <danielsan.robles16@gmail.com>
2022-05-29 02:19:49 -0500

    token finalizado

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 var mysql = require('mysql');
 const PORT = process.env.PORT || 3050;
@@ -6,11 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const jwt = require('jsonwebtoken');
-const { verify } = require('jsonwebtoken');
 
-app.get('/',(req,res) =>{
-    res.send('Welcome to my API!')
-})
+
 //mysql
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -23,56 +19,106 @@ connection.connect(error =>{
     if(error) throw error;
     console.log('Database server running!')
 });
-app.get('/userName/:user',verifyToken,(req,res)=>{
-    const {user} = req.params
-    const sql = `SELECT * FROM usuarios WHERE user = "${user}"`;
-    connection.query(sql,(error,resul) =>{
-        if(error) throw error;
-        if (resul.length > 0 ){
-            res.json(resul);
-        }else{
-            res.send('nel')
-        }
-    });
-    
-//autorisacion token
-function verifyToken(req,res,next){
-    const bearerHeader = req.headers['authorization'];
-
-    if(typeof bearerHeader !== 'undefined'){
-        const bearerToken = bearerHeader.split(" ")[1];
-        req.token = bearerToken;
-    }else{
-        res.sendStatus(403);
-    }
-
-   
-}
-
-})
-app.post('/newUser',(req,res)=>{
-    const sql = 'INSERT INTO usuarios SET ?';
-
-    const usuario_obj = {
-        user : req.body.user,
-        password : req.body.password
+app.post('/login',(req,res)=>{
+    console.log('hola')
+    const login_JSON = {
+        correo : req.body.correo,
+        contrasenia : req.body.contrasenia
     };
 
-    connection.query(sql,usuario_obj,error =>{
+    const sql = `SELECT * FROM usuarios WHERE correo = '${login_JSON.correo}' and contrasenia = '${login_JSON.contrasenia}'`;
+
+    connection.query(sql,(error,resul) =>{
         if(error) throw error;
-     
-        console.log('usuario Created!');
-        jwt.sign({usuario_obj},'secretKey',(err,token)=>{
-            res.json({
-                token
-            })
-        })
+
+            if (resul.length > 0 ){
+                
+                const payload = {
+                    check:true
+                };
+                const token = jwt.sign(payload,'clavesecreta123',{
+                    expiresIn:'7d'
+                });
+                let lista = []
+                let  resultado_token ={}
+                resultado_token.id =  resul[0].id
+                resultado_token.nombre =  resul[0].nombre
+                resultado_token.correo =  resul[0].correo
+                resultado_token.contrasenia =  resul[0].contrasenia
+                resultado_token.token =  token    
+                console.log(resultado_token)
+                lista.push(resultado_token)
+                res.json(lista)
+             
+                res.status(200)
+                // res.json(resul);
+            }else{
+                res.json(['incorrecto'])
+            }
     });
     
+})
+
+app.post('/newUser',(req,res)=>{
+
+    const usuario_obj = {
+        nombre : req.body.nombre,
+        correo : req.body.correo,
+        contrasenia : req.body.contrasenia
+
+    };
+    connection.query('SELECT * FROM usuarios WHERE correo = ?',[usuario_obj.correo],(err,userdata)=>{
+        if(userdata.length > 0){
+
+            res.send('Usuario Existente')
+        }else{
+            
+            connection.query('INSERT INTO usuarios SET ?',usuario_obj,error =>{
+                if(error) throw error;
+             
+                    console.log('usuario Created!');
+                
+                    res.send('true');
+            });
+        }
+    });
+
 });
 
+const verificacion = express.Router()
 
+verificacion.use((req,res,next)=>{
+    let token = req.headers['x-access-token'] || req.headers['authorization']
+    // console.log(token)
+    if(!token){
+        res.status(401).send(
+            {
+                error: 'es necesario un token'
+            }
+        )
+        return
+    }
+    if(token.startsWith('Bearer  ')){
+        token = token.slice(7,token.length);
+        console.log(token)
+    }
+    if(token){
+        jwt.verify(token,app.get('key'),(error,decode)=>{
+            if(error){
+                return res.json({
+                    message: 'el token'
+                })
+            }
+        })
+    }
+          
+});
+
+app.get('/info',verificacion,(req,res)=>{
+    res.json('Informacion importante entregada')
+})
 
 app.listen(3050, function () {
 console.log('La Aplicación está funcionando en el puerto 3050');
+
 });
